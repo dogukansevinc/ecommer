a7200754d57ee96abba1325a5b4ceaba15c41e56
Mehmet Dogukan Sevinc <nextandbackacademy@gmail.com>
2020-10-16 12:39:54 +0300

    Working

--- resources/views/admin/admin_layouts.blade.php
+++ resources/views/admin/admin_layouts.blade.php
@@ -584,20 +584,20 @@
 
 <script src="{{asset('public/backend/lib/jquery/jquery.js')}}"></script>
 <script src="{{asset('public/backend/lib/popper.js/popper.js')}}"></script>
-<script src="../lib/bootstrap/bootstrap.js"></script>
-<script src="../lib/jquery-ui/jquery-ui.js"></script>
-<script src="../lib/perfect-scrollbar/js/perfect-scrollbar.jquery.js"></script>
-<script src="../lib/jquery.sparkline.bower/jquery.sparkline.min.js"></script>
-<script src="../lib/d3/d3.js"></script>
-<script src="../lib/rickshaw/rickshaw.min.js"></script>
-<script src="../lib/chart.js/Chart.js"></script>
-<script src="../lib/Flot/jquery.flot.js"></script>
-<script src="../lib/Flot/jquery.flot.pie.js"></script>
-<script src="../lib/Flot/jquery.flot.resize.js"></script>
-<script src="../lib/flot-spline/jquery.flot.spline.js"></script>
+<script src="{{asset('public/backend/lib/bootstrap/bootstrap.js')}}"></script>
+<script src="{{asset('public/backend/lib/jquery-ui/jquery-ui.js')}}"></script>
+<script src="{{asset('public/backend/lib/perfect-scrollbar/js/perfect-scrollbar.jquery.js')}}"></script>
+<script src="{{asset('public/backend/lib/jquery.sparkline.bower/jquery.sparkline.min.js')}}"></script>
+<script src="{{asset('public/backend/lib/d3/d3.js')}}"></script>
+<script src="{{asset('public/backend/lib/rickshaw/rickshaw.min.js')}}"></script>
+<script src="{{asset('public/backend/lib/chart.js/Chart.js')}}"></script>
+<script src="{{asset('public/backend/lib/Flot/jquery.flot.js')}}"></script>
+<script src="{{asset('public/backend/lib/Flot/jquery.flot.pie.js')}}"></script>
+<script src="{{asset('public/backend/lib/Flot/jquery.flot.resize.js')}}"></script>
+<script src="{{asset('public/backend/lib/flot-spline/jquery.flot.spline.js')}}"></script>
 
-<script src="../js/starlight.js"></script>
-<script src="../js/ResizeSensor.js"></script>
-<script src="../js/dashboard.js"></script>
+<script src="{{asset('public/backend/js/starlight.js')}}"></script>
+<script src="{{asset('public/backend/js/ResizeSensor.js')}}"></script>
+<script src="{{asset('public/backend/js/dashboard.js')}}"></script>
 </body>
 </html>
